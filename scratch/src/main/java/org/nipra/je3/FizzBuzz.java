56dda73a40e2f8056f8848cf1bdef2fe488a675a
Nikhil Prabhakar <nprabhak@nprabhak-mn.local>
2013-06-11 11:37:11 +0530

    FizzBuzz.

--- scratch/src/main/java/org/nipra/je3/FizzBuzz.java
+++ scratch/src/main/java/org/nipra/je3/FizzBuzz.java
@@ -1,5 +1,22 @@
 package org.nipra.je3;
 
+// (nprabhak@nprabhak-mn ~/Projects/Java/Jafun/scratch)$ java -cp target/classes org.nipra.je3.FizzBuzz
 public class FizzBuzz {
-	
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (((i % 5) == 0) && ((i % 7) == 0)) {
+				System.out.print("fizzbuzz");
+			} else if ((i % 5) == 0) {
+				System.out.print("fizz");
+			} else if ((i % 7) == 0) {
+				System.out.print("buzz");
+			} else {
+				System.out.print(i);
+			}
+
+			System.out.print(" ");
+		}
+
+		System.out.println();
+	}
 }
