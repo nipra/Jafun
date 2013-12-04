9696d3e458f9b8b06e9bdbf35341b71cb4a8ab1c
Nikhil Prabhakar <nprabhak@nprabhak-mn.local>
2013-12-04 14:47:46 +0530

    Cascading scratch pad related changes.

--- mr/src/main/java/com/nipra/mr/cascading/impatient/Copy.java
+++ mr/src/main/java/com/nipra/mr/cascading/impatient/Copy.java
@@ -10,6 +10,8 @@ import cascading.pipe.Pipe;
 import cascading.tap.Tap;
 import cascading.tap.hadoop.Hfs;
 
+// hadoop fs -rmr /data/rain.txt
+// hadoop fs -rmr /output/rain
 // (nprabhak@nprabhak-mn ~/Projects/Java/Jafun/mr)$ hadoop fs -put ~/Softwares/Hadoop/Cascading/Impatient/part1/data/rain.txt /data
 // (nprabhak@nprabhak-mn ~/Projects/Java/Jafun/mr)$ mvn clean compile install
 // (nprabhak@nprabhak-mn ~/Projects/Java/Jafun/mr)$ hadoop jar target/mr-0.0.1-SNAPSHOT.jar com.nipra.mr.cascading.impatient.Copy /data/rain.txt /output/rain
